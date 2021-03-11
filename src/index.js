ae10b0c6c87a70075fd3921ae59418e38118abb2
Andrey <aimdragman2@yandex.ru>
2021-03-11 06:56:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let configArray = bracketsConfig.map((arr) => arr.join(""));
+    let newStr = str;
+
+    for (let i = 0; i < configArray.length; i++) {
+        if (str.includes(configArray[i])) {
+            newStr = str.replace(configArray[i], "");
+            i = -1;
+        }
+        str = newStr;
+    }
+
+    return newStr === "" ? true : false;
+};
